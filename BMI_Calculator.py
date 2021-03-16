913dc3754a8ce56f8a186b58468f3e530f3f2699
shlomiDaari <sshlomid@gmail.com>
2021-03-16 19:29:52 -0400

    BMI calculator

---
+++ BMI_Calculator.py
@@ -0,0 +1,27 @@
+#BMI Calculator
+units = int((input("For calculate in kg and m type 1, for calculate in ibs and in type 2:  ")))
+normal_weight = "The normal weight BMI is between 18.5 to 25"
+if units == 1:
+    weight = float(input("What is your weight?(kg) "))
+    hight = float(input("What is your hight?(m) "))
+    BMI = round(weight/hight ** 2)
+    if BMI < 18.5:
+        print(f"You are underweight, your BMI result is {BMI} {normal_weight} ")
+    elif BMI < 25:
+        print(f"You are in normal weight, your BMI result is {BMI} {normal_weight}")
+    elif BMI < 30:
+        print(f"you are overweight, your BMI result is {BMI} {normal_weight}")
+    else:
+        print(f"Obese!, your BMI result is {BMI} {normal_weight}")
+else:
+    weight = float(input("What is your weight?(ibs) "))
+    hight = float(input("What is your hight?(in) "))
+    BMI = float(703*weight/hight ** 2)
+    if BMI < 18.5:
+        print(f"You are underweight, your BMI result is {BMI} {normal_weight} ")
+    elif BMI < 25:
+        print(f"You are in normal weight, your BMI result is {BMI} {normal_weight}")
+    elif BMI < 30:
+        print(f"you are overweight, your BMI result is {BMI} {normal_weight}")
+    else:
+        print(f"Obese!, your BMI result is {BMI} {normal_weight}")
